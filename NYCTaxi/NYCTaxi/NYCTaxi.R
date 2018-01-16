30cb080b43c530241ff90f5f1895953abd5278ba
Shivam Verma <shivamv@maqsoftware.net>
2018-01-16 20:46:20 +0530

    Office work

---
+++ NYCTaxi/NYCTaxi/NYCTaxi.R
@@ -0,0 +1,38 @@
+#######################################
+# Library
+#######################################
+
+library(data.table)
+
+library(tibble)
+
+library(dplyr)
+
+library(lubridate)
+
+#######################################
+# Load data into frames
+#######################################
+
+train <- as.tibble(fread("D:/Kaggle/train/train.csv"))
+
+test <- as.tibble(fread("D:/Kaggle/test/test.csv"))
+
+#######################################
+# Handling Missing Values (NA's)
+#######################################
+
+table(is.na(train))
+
+table(is.na(test))
+
+#######################################
+# Converting into factors
+#######################################
+
+train <- mutate(train, pickup_datetime = ymd_hms(pickup_datetime), dropoff_datetime = ymd_hms(dropoff_datetime),
+	vendor_id = factor(vendor_id), passenger_count = factor(passenger_count))
+
+#######################################
+# 
+#######################################
